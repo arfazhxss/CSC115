1164486d8b6042e8ef55d9d3a552ce2c63354b96
arfazhussain <arfazhussain@users.noreply.github.com>
2022-07-27 22:31:53 -0700

    update jul-27

---
+++ B Main()/A10.6 Mid-3/B.java
@@ -0,0 +1,12 @@
+public class B extends A implements Comparable<A> {
+    public B() {
+        System.out.println("B constructor 1");
+    }
+    public B (int x) {
+        super(x);
+        System.out.println("B constructor 2");
+    }
+    public void otherStuff() {
+        System.out.println("B's other stuff: "+x);
+    }
+}
---
+++ B Main()/A10.6 Mid-3/midThreeTests.java
@@ -0,0 +1,9 @@
+public class midThreeTests {
+    public static void main (String[] args) {
+        // String first = "computer";
+        // String second = "science";
+        // System.out.println(first.compareTo(second)>0);
+        B ex2 = new B(2); 
+        ex2.otherStuff();
+    }
+}
